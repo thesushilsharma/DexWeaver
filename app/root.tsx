df7e8b524711f3f9b2acbe217ff3268a55658248
Sushil Sharma <58214222+thesushilsharma@users.noreply.github.com>
2025-01-07 01:55:57 +0400

    adding Hydration template

--- app/root.tsx
+++ app/root.tsx
@@ -46,6 +46,18 @@ export default function App() {
   return <Outlet />;
 }
 
+export function HydrateFallback() {
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <div className="relative inline-flex">
+        <div className="h-5 w-5 mr-3 bg-sky-500 rounded-full"></div>
+        <div className="h-5 w-5 mr-3 bg-sky-500 rounded-full absolute top-0 left-0 animate-ping"></div>
+        <div className="h-5 w-5 mr-3 bg-sky-500 rounded-full absolute top-0 left-0 animate-pulse"></div>
+      </div>
+    </div>
+  )
+}
+
 export function ErrorBoundary({ error }: Route.ErrorBoundaryProps) {
   let message = "Oops!";
   let details = "An unexpected error occurred.";
